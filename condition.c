2dd6a827235ec75984efa1258bd244b91ee66263
길형준 <gilhyeongjun@gilhyeongjun-ui-MacBookPro.local>
2021-02-14 22:31:30 +0900

    condition practice modify

--- condition.c
+++ condition.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void) 
 {
-    printf("Heelo world!!");
+    int chance = 20;
+    srand(time(NULL));
+    int intRand = rand() % 100 + 1;
+    int answer;
+
+    while (chance > 0)
+    {
+        printf("남은 기회 : %d \n" , chance);
+        scanf("%d", &answer);
+
+        if(answer > intRand) 
+        {
+            printf("입력 값이 더 큽니다. \n" );
+        }
+        else if (answer < intRand)
+        {
+            printf("입력 값이 더 작습니다.");
+        }
+        else if (answer == intRand) 
+        {
+            printf("정답입니다.");
+        }
+
+        chance -- ;
+    };
+    
+
+
+
 }
